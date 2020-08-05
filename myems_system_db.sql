08f695cb3ced05600daea639876ac3a6be2a052c
guangyuzhang <13621160019@163.com>
2020-08-05 11:33:03 +0800

    removed timezone_id from tbl_shopfloors

--- myems_system_db.sql
+++ myems_system_db.sql
@@ -982,7 +982,6 @@ CREATE TABLE IF NOT EXISTS `myems_system_db`.`tbl_shopfloors` (
   `name` VARCHAR(255) NOT NULL,
   `uuid` CHAR(36) NOT NULL,
   `area` DECIMAL(18, 3) NOT NULL,
-  `timezone_id` BIGINT NOT NULL,
   `is_input_counted` BOOL NOT NULL,
   `contact_id` BIGINT,
   `cost_center_id` BIGINT,
@@ -997,9 +996,9 @@ START TRANSACTION;
 USE `myems_system_db`;
 
 INSERT INTO `myems_system_db`.`tbl_shopfloors`
-(`id`, `name`, `uuid` , `area`, `timezone_id`, `contact_id`, `is_input_counted`,  `cost_center_id`, `description`)
+(`id`, `name`, `uuid` , `area`, `contact_id`, `is_input_counted`,  `cost_center_id`, `description`)
 VALUES
-    (1, 'MyEMS Shopfloor', 'd03837fd-9d30-44fe-9443-154f7c7e15f1',  99999.999, 56, 1, true, 1,  'MyEMS Project');
+    (1, 'MyEMS Shopfloor', 'd03837fd-9d30-44fe-9443-154f7c7e15f1',  99999.999, 1, true, 1,  'MyEMS Project');
 COMMIT;
 
 
